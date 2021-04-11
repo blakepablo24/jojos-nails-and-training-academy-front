fd843099e25e704d4a5cd70b9354cb934cee38fb
Paul Robson <blakepablo24@gmail.com>
2021-04-11 14:56:06 +0100

    added basket to Snippets

--- src/components/Snippet/Snippet.js
+++ src/components/Snippet/Snippet.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './Snippet.module.css';
+import { TiShoppingCart } from "react-icons/ti";
 
 const snippet = (props) => {
 
     let snippetLink = '/single-training-course/'; 
+    let shoppingControlContainer = "";
 
     if(props.salonTreatment) {
         snippetLink = '/category/' + props.title.replace(/\s+/g, '-').replace(/,/g,"").toLowerCase() + '/';
@@ -12,12 +14,17 @@ const snippet = (props) => {
 
     if(props.salonTreatmentSubCat) {
         snippetLink = '/treatment/' + props.title.replace(/\s+/g, '-').replace(/,/g,"").toLowerCase() + '/';
+        shoppingControlContainer = 
+        <div className={classes.basketButtonContainer}>
+            <TiShoppingCart />
+        </div>
     }
 
     return (
         <Link to={snippetLink + props.id} className={classes.Snippet}>
             <img src={props.image} alt="" />
             <h3>{props.title}</h3>
+            {shoppingControlContainer}
         </Link>
     )
 }
