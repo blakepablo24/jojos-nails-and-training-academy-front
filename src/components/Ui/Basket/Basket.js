03a212317e386adf6c237d647142b26b820a5d1a
Paul Robson <blakepablo24@gmail.com>
2021-04-20 16:04:12 +0100

    Added totals functionality in basket

--- src/components/Ui/Basket/Basket.js
+++ src/components/Ui/Basket/Basket.js
@@ -7,16 +7,27 @@ import BasketItem from './BasketItem/BasketItem';
 const basket = (props) => {
     let attachedClasses = [classes.Basket, classes.Hide];
     let totalCost = 0;
-    let currentBasketItems = "There are currently no items in your basket";
-    
+    let currentBasketItems = <h2 className={classes.noItemsInBasket}>There are currently no items in your basket</h2>;
+
     if(props.itemsInBasket.length > 0){
+        let priceArray = [];
+        let price = 0;
+        props.itemsInBasket.forEach(item => {
+            price = item.price * item.quantity;
+            priceArray.push(price);
+
+            return priceArray
+        });
+
+        totalCost = priceArray.reduce(function(pv, cv) { return pv + cv; });
+        
         currentBasketItems =
         props.itemsInBasket.map(item =>
             <BasketItem
                 id={item.id}
                 key={item.id}
                 title={item.title}
-                price={item.price}
+                price={item.price * item.quantity}
                 quantity={item.quantity}
                 remove={props.remove}
                 minus={props.minus}
@@ -41,7 +52,7 @@ const basket = (props) => {
                     <p>Total: £ {totalCost}</p>
                 </div>
                 <div className={classes.basketControlsContainer}>
-                    <button className={classes.bookTreatmentsButton}>Continue Shopping</button>
+                    <button className={classes.bookTreatmentsButton} onClick={props.toggleBasket}>Continue Shopping</button>
                     <button className={classes.bookTreatmentsButton}>Book treatments</button>
                 </div>
             </div>
--- src/components/Ui/Basket/BasketItem/BasketItem.js
+++ src/components/Ui/Basket/BasketItem/BasketItem.js
@@ -5,12 +5,6 @@ import classes from './BasketItem.module.css';
 
 const basketItem = (props) => {
 
-    let price = 0;
-
-    if(props.price){
-        price = props.price * props.quantity;
-    }
-
     return(
         <div className={classes.basketItem}>
             <p className={classes.basketItemTitle}>{props.title}</p>
@@ -19,7 +13,7 @@ const basketItem = (props) => {
                 <p>{props.quantity}</p>
                 <BiPlusCircle onClick={props.plus.bind(this, props.id, props.title, props.price)}/>
             </div>
-            <p>£ {price}</p>
+            <p>£ {props.price}</p>
             <TiDeleteOutline color="red" onClick={props.remove.bind(this, props.id)} />
         </div>
     )
