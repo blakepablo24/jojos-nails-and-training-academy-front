5111a153d83b798d877af9563c84565687071010
Paul Robson <Paul Robson>
2021-07-01 15:07:58 +0100

    continued work on voucher purchase gui

--- src/containers/FindUs/FindUs.js
+++ src/containers/FindUs/FindUs.js
@@ -17,7 +17,7 @@ class FindUs extends Component {
                 <Latest message="Come and Visit the Salon" />
                 <div className={classes.FindUs}>
                     <Map location={location} zoomLevel={17}/>
-                    <div className="elfsight-app-46f51913-5254-4ec0-b0c4-7ecafd184bf2"></div>
+                    {/* <div className="elfsight-app-46f51913-5254-4ec0-b0c4-7ecafd184bf2"></div> */}
                 </div>
             </Aux>
         )
--- src/containers/GiftVouchers/GiftVouchers.js
+++ src/containers/GiftVouchers/GiftVouchers.js
@@ -7,7 +7,10 @@ import Logo from "../../components/Ui/Navigation/Header/Logo/Logo";
 class GiftVouchers extends Component {
 
     state = {
-        value: 50
+        value: 25,
+        to: "",
+        from: "",
+        stage: 1
     }
 
     onChangeHandler = (event) => {
@@ -22,30 +25,90 @@ class GiftVouchers extends Component {
         });
     }
 
+    changeToPreviewHandler = (stage) => {
+        this.setState({
+            stage: stage
+        })
+    }
+
     render(){
+
+        let instructions = <p className={classes.instructions}>&#129044; Use this slider to select your voucher value</p>;
+
+        if(this.state.value !== 25){
+            instructions = "";
+        }
+
+        let button = "";
+
+        let voucherGui =    <div className={classes.voucherWizardContainer}>
+                                <div className={classes.voucherValueContainer}>
+                                    {instructions}
+                                    <input 
+                                        type="range"
+                                        value={this.state.value}
+                                        name="value"
+                                        min="25"
+                                        max="500"
+                                        step="25"
+                                        onChange={this.onChangeHandler} 
+                                        className={classes.rangeSlider}
+                                    />
+                                </div>
+                                <p className={classes.exampleValue}>£{this.state.value}</p>
+                                <div className={classes.voucherNameInputContainer}>
+                                    <input
+                                        type="text"
+                                        value={this.state.to}
+                                        name="to" 
+                                        placeholder="Enter Voucher To Name?" 
+                                        onChange={this.onChangeHandler}
+                                        className={classes.voucherNameInput}
+                                    />
+                                    <input
+                                        type="text"
+                                        value={this.state.from}
+                                        name="from" 
+                                        placeholder="Enter Voucher From Name?" 
+                                        onChange={this.onChangeHandler}
+                                        className={classes.voucherNameInput}
+                                    />
+                                </div>
+                            </div>
+
+        if(this.state.to && this.state.from) {
+            button =    <div className={classes.buttonContainer}>
+                            <button className="customButton" onClick={this.changeToPreviewHandler.bind(this, 2)}>Next</button>
+                        </div>
+        }                            
+
+        if(this.state.stage === 2) {
+            button =    <div className={classes.buttonContainer}>
+                            <button className="customButton" onClick={this.changeToPreviewHandler.bind(this, 1)}>Back</button>
+                            <button className="customButton" onClick={this.changeToPreviewHandler.bind(this, 3)}>Buy Now</button>
+                        </div>
+        }             
+
+        if(this.state.stage === 2){
+            voucherGui =   <div className={classes.voucherExample}>
+                                <div className={classes.voucherImage}>
+                                    <Logo />
+                                </div>
+                                <h2 className={classes.voucherTitle}>£{this.state.value} Gift Voucher</h2>
+                                <h3>to</h3>
+                                <h3 className={classes.voucherName}>{this.state.to}</h3>
+                                <h3>from</h3>
+                                <h3 className={classes.voucherName}>{this.state.from}</h3>
+                                <p className={classes.voucherCode}>Code: XXXX-XXXX-XXXX</p>
+                            </div>
+        }
+
         return(
             <Aux>
-                <Latest message="Vouchers from £25" />
+                <Latest message="Digital Gift Vouchers from £25" />
                 <div className={classes.GiftVouchers}>
-                    <div className={classes.voucherWizardContainer}>
-                    <p>Use the slider to select your</p>
-                        <div className={classes.voucherValue}>
-                            <input 
-                                type="range"
-                                value={this.state.value}
-                                name="value" 
-                                min="25"
-                                max="250"
-                                step="25"
-                                onChange={this.onChangeHandler} 
-                                className={classes.rangeSlider}
-                            />
-                        </div>
-                        <p>£{this.state.value}</p>
-                        <div className={classes.voucherName}>
-                            <input placeholder="Name" />
-                        </div>
-                    </div>
+                    {voucherGui}
+                    {button}
                 </div>
             </Aux>
         )
